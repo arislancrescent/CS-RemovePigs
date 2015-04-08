209694a7f3e29fb3a66e30f85ac8b87929b1cd2a
Aris Lancrescent <Aris@Ariss-MacBook-Pro.local>
2015-04-08 20:19:29 +0200

    Initial commit

---
+++ Identity.cs
@@ -0,0 +1,17 @@
+﻿using ICities;
+
+namespace RemovePigs
+{
+    public class Identity : IUserMod
+    {
+        public string Name
+        {
+            get { return Settings.Instance.Tag; }
+        }
+
+        public string Description
+        {
+            get { return "Nonpermanently removes all pigs. Disable to get the pigs back. Requires [ARIS] Skylines Overwatch."; }
+        }
+    }
+}
