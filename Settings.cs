cecb4d4081436c6f5a6c9a24ac25b373b9960b4e
Aris Lancrescent <Aris@Ariss-MacBook-Pro.local>
2015-04-10 12:56:04 +0200

    PTR build configuration added

--- Settings.cs
+++ Settings.cs
@@ -6,12 +6,43 @@ namespace RemovePigs
     {
         private Settings()
         {
-            Tag = "[ARIS] Remove Pigs";
+            #if DEBUG
+
+            IsDebugBuild   = true;
+            IsPTRBuild     = false;
+            IsReleaseBuild = false;
+
+            Flair         += "ARIS LOCAL";
+
+            #elif PTR
+
+            IsDebugBuild   = false;
+            IsPTRBuild     = true;
+            IsReleaseBuild = false;
+
+            Flair         += "ARIS PTR";
+
+            #else
+
+            IsDebugBuild   = false;
+            IsPTRBuild     = false;
+            IsReleaseBuild = true;
+
+            Flair         += "ARIS";
+
+            #endif
+
+            Tag = String.Format("[{0}] Remove Pigs", Flair);
         }
 
         private static readonly Settings _Instance = new Settings();
         public static Settings Instance { get { return _Instance; } }
 
+        public readonly string Flair;
         public readonly string Tag;
+
+        public readonly bool IsDebugBuild;
+        public readonly bool IsPTRBuild;
+        public readonly bool IsReleaseBuild;
     }
 }
