7d0774544b8616ece51e0feee93ae1e224fd9792
Aris Lancrescent <Aris@Ariss-MacBook-Pro.local>
2015-05-09 11:18:38 +0200

    Added NotifyPlayer(), which uses the game's debug facilities. Log() will now use Unity's debug facilities.

--- Helper.cs
+++ Helper.cs
@@ -26,7 +26,13 @@ namespace RemovePigs
 
         public void Log(string message)
         {
+            Debug.Log(String.Format("{0}: {1}", Settings.Instance.Tag, message));
+        }
+
+        public void NotifyPlayer(string message)
+        {
             DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, String.Format("{0}: {1}", Settings.Instance.Tag, message));
+            Log(message);
         }
     }
 }
